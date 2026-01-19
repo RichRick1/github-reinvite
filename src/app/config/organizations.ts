a86d1ebc7a6ce6c69a76a36bd7594da565ae16b5
= <valerachuiko@gmail.com>
2026-01-19 09:34:20 -0500

    added assignments

--- src/app/config/organizations.ts
+++ src/app/config/organizations.ts
@@ -8,7 +8,25 @@ export const ORGANIZATIONS = {
       "🧑GitHub Fundamentals": "github-fundamentals",
       "Moments Demo": "easy-moments",
       "Moment of PinBox": "pinbox-moments",
-      "2 Particles in a Box": "2pinbox"
+      "2 Particles in a Box": "2pinbox",
+      "Effective 1d Systems": "effective-1d",
+      "Gaussian Well": "gaussian-well",
+      "Quiz: MultiD PinBox": "quiz-multidpinbox",
+      "Quiz: Particle in 1D Box": "quiz-particle-in-1d-box",
+      "Quiz: Postulations": "quiz-postulations",
+      "Quiz: Schrodinger Equation": "quiz-schrodinger-equation",
+      "Sudden Approximation": "sudden-approximation",
+      "Sudden Approximation 2D": "sudden2d",
+      "Tilted PinBox": "tilted-pinbox",
+      "Quiz: Demo": "demo-quiz",
+      "Grading": "grading",
+      "Quiz: Introduction to Quantum Mechanics": "questions-introduction",
+      "Schrodinger Cat": "schrodinger-cat",
+      "Wavefunction Normalization": "wavefunction-normalization",
+      "Hydrogenic Atoms": "hydrogenic-atoms",
+      "Knowledge Test 1": "knowledge-test-1",
+      "Knowledge Test 2": "knowledge-test-2",
+      "Knowledge Test 3": "knowledge-test-3"
     }
   }
 } as const;
